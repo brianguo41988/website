a1f2f593a0b68f1ebc0322b26b62bbf97668494f
brianguo41988 <brianguo@live.unc.edu>
2021-06-28 19:48:43 -0400

    added the port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 
-//var PORT = process.env.PORT || 5000;
+var PORT = process.env.PORT || 5000;
 
 app.get('/', function (req, res) {
     res.sendfile(__dirname + '/index.html');
