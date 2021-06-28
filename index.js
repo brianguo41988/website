0276fc5100d67bd15879038e6e6a5d1022bb7815
brianguo41988 <brianguo@live.unc.edu>
2021-06-28 19:46:53 -0400

    should fully run on heroku now

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+
 const express = require('express');
 
 const app = express();
 
-var PORT = process.env.PORT || 5000;
+//var PORT = process.env.PORT || 5000;
 
 app.get('/', function (req, res) {
     res.sendfile(__dirname + '/index.html');
   });
-  
+  app.get('/hi', function (req, res) {
+    res.sendfile(__dirname + '/index.html');
+  });
 console.log("Entered");
