0f83904c52d46bcee9cfcd96c829f23994d8ba06
brianguo41988 <brianguo@live.unc.edu>
2021-06-27 22:27:38 -0400

    added exopress

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+const express = require('express');
+
+const app = express();
+
 var PORT = process.env.PORT || 5000;
+
 app.get('/', function (req, res) {
     res.sendfile(__dirname + '/index.html');
   });
+  
 console.log("Entered");
